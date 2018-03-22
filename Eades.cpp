2a7be8d185d1a4cd94396ae46ca238af527ef010
MilkTea <xtej3g@gmail.com>
2018-03-22 19:49:40 +0900

    🐾

--- Eades.cpp
+++ Eades.cpp
@@ -22,7 +22,6 @@ struct Node{
     Point rectangle;
     Point velocity;
     Point force;
-    vector<int> negihbor;
 
     void Eular(double dt, Point force){
         rectangle.x += dt * velocity.x;
@@ -35,13 +34,14 @@ struct Node{
         
         double k = 0.1;
         double l = 60.0;
-        double dx = this.rectangle.x - another.rectangle.x;
-        double dy = this.rectangle.y - another.rectangle.y;
+        double dx = rectangle.x - another.rectangle.x;
+        double dy = rectangle.y - another.rectangle.y;
         double d2 = dx * dx + dy + dy;
         
         if(d2 < 0.001){
             force.x = (double)rand() / RAND_MAX - 0.5;
             force.y = (double)rand() / RAND_MAX - 0.5;
+            return;
         }
 
         double d = sqrt(d2);
@@ -53,54 +53,46 @@ struct Node{
         force.y = -k * dl * sin;
     };
 
-    void ResilienceForce(Node another){
-
-        double g = 500.0;
-        dx = this.rectangle.x - anothe.rectangle.x;
-        dy = this.rectangle.y - anothe.rectangle.y;
-        double d2 = dx * dx + dy + dy;
-        
-        if(d2 < 0.001){
-            force.x = (double)rand() / RAND_MAX - 0.5;
-            force.y = (double)rand() / RAND_MAX - 0.5;
-        }
-
-        double d = sqrt(d2);
-        double cos = dx / d;
-        double sin = dy / d;
-        double dl = d - l;
-
-        force.x = g / d2 * cos;
-        force.y = g / d2 * sin;
-    };
-
     void FrictionalForce(Node another){
         double m = 0.3;
         force.x = -m * velocity.x;
         force.y = -m * velocity.y;
     }
+
+    void Move(vector<Node> &other){
+
+        int i;
+        for(i = 0; i < other.size(); i++){
+            SpringForce(other[i]);
+            FrictionalForce(other[i]);
+        }
+    }
 };
 
 int main(){
 
-    int i;
+    int i, j, k;
     vector<Node> data(6);
-    data[0].id = 1;
-    data[0].neighbor.push_back(2);
-    data[0].neighbor.push_back(3);
-    data[1].id = 2;
-    data[1].neighbor.push_back(1);
-    data[2].id = 3;
-    data[2].neighbor.push_back(1);
-    data[2].neighbor.push_back(4);
-    data[2].neighbor.push_back(5);
-    data[3].id = 4;
-    data[3].neighbor.push_back(3);
-    data[4].id = 5;
-    data[4].neighbor.push_back(3);
-    data[4].neighbor.push_back(6);
-    data[5].id = 6;
-    data[5].neighbor.push_back(5);
+
+    // 初期化
+    for(i = 0; i < data.size(); i++){
+        data[i].id = i;
+    }
+
+    // バネモデル実行
+    for(i = 0; i < 6; i++){
+        for(j = 0; j < 6; j++){
+            if(i != j){
+                data[i].SpringForce(data[j]);
+                data[i].Eular(0.01, data[j].force);             
+            }
+        }
+    }
+
+    // 出力
+    for(i = 0; i < 6; i++){
+        cout << i << " " << data[i].rectangle.x << " " << data[i].rectangle.y << endl;
+    }
 
     return 0;
 }
